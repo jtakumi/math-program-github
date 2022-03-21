9a0f8b47a33d20f85c9e5f47cd03d9654874bbb8
j____takumi <takumi87jn@gmail.com>
2022-03-21 20:27:51 +0900

    logit

---
+++ logit.py
@@ -0,0 +1,21 @@
+import imp
+import math
+
+import pandas as pd
+import numpy as np
+from matplotlib import pylab as plt
+from scipy.special import expit
+import japanize_matplotlib 
+
+def logit(x):
+    return np.log(x/(1-x))
+
+data=list()
+points=[4,8,15,16,23,42]
+
+for a in points:
+    data.append(logit(expit(a)))
+
+df=pd.DataFrame(data,index=points,columns=['標準シグモイド関数->ロジット'])
+df.to_csv('../math-program-github/logit.csv')
+print(df)
